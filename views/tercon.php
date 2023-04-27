4c6bfc1495f841c832e6ae7ea5b920f438ae750c
terco89 <valeraquiroz.gb@gmail.com>
2023-04-27 17:32:19 -0300

    politicas de privacidad

--- views/tercon.php
+++ views/tercon.php
@@ -4,16 +4,16 @@
             <h1>Terminos y condiciones</h1>
             <div>
 Los siguientes términos y condiciones de uso rigen el uso del launcher de juegos y todos los contenidos que se encuentren en él. Al utilizar el launcher de juegos, usted acepta estos términos y condiciones de uso. Si no acepta estos términos y condiciones de uso, no utilice el launcher de juegos.
-<br><br><h2>Contenido del launcher de juegos</h2>
+<br><br><h3>Contenido del launcher de juegos</h3>
 El launcher de juegos puede contener, entre otros, los siguientes contenidos:
-<list>
+<br><br><list>
 <li>Juegos</li>
 <li>Imágenes y videos promocionales</li>
 <li>Textos descriptivos</li>
 <li>Comentarios de otros usuarios</li>
 <li>Herramientas para comunicarse con otros usuarios</li>
 </list><br><br>
-<h2>Propiedad intelectual</h2>
+<h3>Propiedad intelectual</h3>
 El launcher de juegos reconoce y protege los derechos de propiedad intelectual de terceros y espera que los usuarios hagan lo mismo. Todos los derechos de propiedad intelectual sobre los contenidos del launcher de juegos, incluidos, entre otros, los derechos de autor, marcas registradas y patentes, son propiedad del launcher de juegos o de sus licenciantes.
 Los usuarios del launcher de juegos deben respetar y cumplir con todas las leyes de propiedad intelectual y derechos de autor aplicables. Queda estrictamente prohibido cualquier uso no autorizado de los contenidos del launcher de juegos, incluidos, entre otros, la copia, distribución, modificación, exhibición pública, transmisión, reutilización y cualquier otra forma de explotación de los mismos sin la autorización previa y por escrito del launcher de juegos o de sus licenciantes.<br><br>
 Usted reconoce y acepta que no adquiere ningún derecho de propiedad intelectual sobre los contenidos del launcher de juegos al utilizarlo, excepto por los derechos de uso limitados expresamente otorgados en estos términos y condiciones de uso. Cualquier uso no autorizado de los contenidos del launcher de juegos constituye una violación de los derechos de propiedad intelectual del launcher de juegos y/o de sus licenciantes, y puede resultar en consecuencias legales graves.<br><br>
@@ -22,7 +22,7 @@ El launcher de juegos se reserva el derecho de retirar el acceso a cualquier con
 
 
 
-<br><h2>Uso del contenido del launcher de juegos</h2><br>
+<br><h3>Uso del contenido del launcher de juegos</h3>
 Al utilizar el contenido del launcher de juegos, usted se compromete a no realizar ninguna de las siguientes acciones:<br><br>
 <list>
 <li>Copiar, modificar, distribuir o transmitir el contenido del launcher de juegos o cualquier parte del mismo sin el permiso expreso por escrito del launcher de juegos.</li>
@@ -39,14 +39,14 @@ Al utilizar el contenido del launcher de juegos, usted se compromete a no realiz
 <li>La información en su cuenta y su foto de perfil no deben incitar el odio contra otras personas/grupos, tampoco deben ser inapropiadas(contenido sexual y/o violento).</li>
 </list><br><br>
 
-<h2>Limitación de responsabilidad:</h2>
+<h3>Limitación de responsabilidad:</h3>
 El launcher de juegos y todos sus contenidos se proporcionan "tal cual" sin garantía de ningún tipo, ya sea expresa o implícita. El launcher de juegos no será responsable de ningún daño directo, indirecto, especial, incidental o consecuente derivado del uso del launcher de juegos o sus contenidos, incluidos, entre otros, los daños por pérdida de beneficios, interrupción de negocios, pérdida de información o datos, o daños a la propiedad.<br><br>
 Es importante destacar que si usted comparte las credenciales de su cuenta, y un tercero accede a la cuenta con estas, la empresa no se hace cargo de las posibles alteraciones de la misma. Por lo tanto, es responsabilidad exclusiva del usuario mantener sus credenciales seguras y no compartirlas con terceros.<br><br>
 Asimismo, la empresa no se hace cargo del lenguaje indebido en los chats, ya que estos son medios de comunicación libres y abiertos para la interacción entre usuarios. Cada usuario es responsable de su propio comportamiento y uso adecuado de los chats.<br><br>
 En caso de que el usuario decida eliminar su cuenta y no haya podido recuperarla dentro del plazo de recuperación establecido, no podrá reclamar nada asociado a la cuenta, ya que se establece un plazo límite para la recuperación de la misma y es responsabilidad del usuario asegurarse de cumplir con este plazo.<br><br>
 <br>
 
-<h2>Modificaciones de los términos y condiciones de uso:</h2><br>
+<h3>Modificaciones de los términos y condiciones de uso:</h3>
 El launcher de juegos se reserva el derecho de modificar estos términos y condiciones de uso en cualquier momento, sin previo aviso ni consentimiento de los usuarios. Las modificaciones se harán efectivas inmediatamente después de la publicación de los términos y condiciones de uso modificados en el sitio web del launcher de juegos. Por lo tanto, se recomienda encarecidamente a los usuarios que revisen periódicamente los términos y condiciones de uso para mantenerse informados sobre cualquier cambio que pueda haber sido realizado.<br><br>
 El launcher de juegos se reserva el derecho de modificar estos términos y condiciones de uso por varias razones, incluyendo pero no limitado a cambios en la ley aplicable, cambios en la política de privacidad de la empresa, cambios en los servicios ofrecidos por el launcher de juegos, o simplemente para mantener la coherencia y la eficacia en la utilización del launcher de juegos. Es importante que los usuarios entiendan que el launcher de juegos se reserva el derecho de modificar estos términos y condiciones de uso en cualquier momento y sin previo aviso.<br><br>
 En caso de que se realicen modificaciones a los términos y condiciones de uso, los usuarios serán notificados a través de una publicación en el sitio web del launcher de juegos, donde se detallarán los cambios realizados y su efectividad inmediata. Es responsabilidad exclusiva del usuario revisar los términos y condiciones de uso regularmente y estar al tanto de cualquier cambio que pueda haber sido realizado.<br><br>
