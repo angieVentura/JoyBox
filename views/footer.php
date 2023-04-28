af0e39224d7f77f7a9e5d26c13c0065956707722
MamushkaDead <gasparkappou@gmail.com>
2023-04-28 14:57:26 -0300

    Footer en la pagina relocona

---
+++ views/footer.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <h5>Sobre nosotros</h5>
+            <p>Somos un pasionado equipo de gamers que le encanta crear y jugar videojuegos.</p>
+        </div>
+        <div class="col-md-4">
+            <h5>Contacto</h5>
+            <p>Email: gamewise26@gmail.com</p>
+            <p>Teléfono: +54 9 11 XXXX-XXXX</p>
+        </div>
+        <div class="col-4 container p-0" id="redes_menu">
+            <div class="row">
+                <div class="col-1 p-0 redes_menu bi bi-tiktok"></div>
+                <div class="col-1 p-0 redes_menu bi bi-instagram"></div>
+                <div class="col-1 p-0 redes_menu bi bi-twitter"></div>
+                <div class="col-7 p-0">
+                    <button id="btn_descargar_footer">Descargar</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <p class="text-center">&copy; 2023 GameWise. All rights reserved.</p>
+        </div>
+    </div>
+</div>
