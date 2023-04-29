b8480d7fe74834e03d90f58595e640278d854889
luishi1 <luis.oviedo.et26@gmail.com>
2023-04-28 22:12:44 -0300

    creo rama l_sn y subo un avance INSIGNIFICANTE

---
+++ views/sobre_nosotros.php
@@ -0,0 +1,28 @@
+
+<div class="container-fluid-2">
+<h1 class="titulosn">SOMOS GAMEWISE</h1>
+<p class="textossn">Una empresa de desarrollo de videojuegos
+que cuenta con un equipo de trabajo que
+trabaja duro para crear los <span class="colorin"> mejores juegos </span>
+de alta calidad, cautivadores y para todo 
+publico </p>
+</div>
+
+<div class="container-fluid">
+<h1 class="titulosn2">NUESTRO SERVICIO</h1>
+<p class="textossn">Ofrecemos nuestra nueva aplicación
+de videojuegos en línea <span class="colorin"> JoyBox </span> . Un servicio donde podrás jugar solo o con tus amigos , 
+crear comunidades , hablar por chat  y un montón de cosas mas</p>
+</div>
+
+
+<div class="container-fluid-2">
+<h1 class="titulosn2">NUESTRO OBJETIVO</h1>
+<p class="textossn">Ofrecer entretenimiento y diversión a nuestros usuarios a través de pequeñas experiencias de juegos sencillos 
+    y divertidos, sin una historia larga y tediosa, como los clásicos videojuegos casuales . 
+    Por que con <span class="colorin"> JoyBox </span>  tenes el camino mas corto hacia la diversion</p>
+</div>
+
+<div class="container-fluid-3">
+<h1 class="titulosn">CONOCE AL EQUIPO</h1>
+</div>
