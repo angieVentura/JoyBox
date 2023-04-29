fd50345fc32dabeb8556cb59e13580d4a20a4bc3
angieVentura <angiev.d.23@gmail.com>
2023-04-29 19:29:53 -0300

    Menu css

--- views/menu.php
+++ views/menu.php
@@ -1,4 +1,4 @@
-<div class="container-fluid p-0 m-0 #fafafa" id="menu">
+<!--<div class="container-fluid p-0 m-0 #fafafa" id="menu">
     <div class="row ">
         <div class="col-2 p-0">
             <a href="index.php"><img src="img/logo.png" id="logo_menu" alt=""></a>
@@ -32,3 +32,38 @@
     <div class="redes_menu"><img src="img/logo_ig.png" class="img_redes_menu" id="ig" alt=""><img src="img/logo_tt.png" class="img_redes_menu" alt=""><img src="img/logo_tw.png" class="img_redes_menu" alt=""></div>
     <div id="btn_descargar">Descargar</div>
 </div>
+-->
+
+<nav class="navbar navbar-expand-lg navbar-light  navbar-custom" aria-label="Fifth navbar example">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+            <div class="img_logo">
+                <img src="img/logo_JoyBox.png" alt="JoyBox" class="logo">
+                <img src="img/JoyBox.png" alt="" class="n_logo">
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExample05">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link activo" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link no-activo" aria-current="page" href="#">Sobre Nosotros</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link no-activo" aria-current="page" href="#">Contacto</a>
+                </li>
+            </ul>
+            <div class="redes">
+                <i class="redes_menu bi bi-instagram"></i>
+                <i class="redes_menu bi bi-tiktok red"></i>
+                <i class="redes_menu bi bi-twitter"></i>
+            </div>
+
+            <button type="button" class="btn btn-outline-light descarga-bnt">Descargar</button>
+        </div>
+    </div>
+</nav>
