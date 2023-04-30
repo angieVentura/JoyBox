2143a333fee36a4483b8914d219c8704d68a5522
angieVentura <angiev.d.23@gmail.com>
2023-04-30 01:11:38 -0300

    Avance del inicio

--- views/home.php
+++ views/home.php
@@ -1,12 +1,25 @@
-
-<div class="contenedor-imagen">
-<img src="../JoyBox/img/mancha_r.png" alt="Mancha roja" class="mancha-roja">
-</div>
-<div class="contenedor-inicio">
-    <div class="text-inicio">
-        <p>DONDE COMIENZA <br>TU DIVERSÍON</p>
+<div class="seccion-dados">
+    <div class="contenedor-imagen">
+        <img src="../JoyBox/img/mancha_r.png" alt="Mancha roja" class="mancha-roja">
     </div>
-    <div class="img-inicio">
-        <img src="../JoyBox/img/dados.png" alt="" class="dados">
+    <div class="contenedor-inicio">
+        <div class="text-inicio">
+            <h1>DONDE COMIENZA <br>TU DIVERSÍON</h1>
+        </div>
+        <div class="img-inicio">
+            <img src="../JoyBox/img/dados.png" alt="" class="dados">
+        </div>
+    </div>
+</div>
+
+<div class="seccion-cartas">
+    <div class="contenedor-inicio-cartas">
+        <div class="img-inicio-cartas">
+            <img src="../JoyBox/img/mancha_negra.png" alt="" class="cartas">
+        </div>
+        <div class="text-inicio-cartas">
+            <h2 class="cartas">¿QUÉ ES JOYBOX?</h2>
+            <p class="carta">JoyBox es una app de emocionantes juegos de amplia variedad. Podrás tener acceso a los mejores juegos en una sola plataforma fácil de usar.</p>
+        </div>
     </div>
 </div>
--- views/menu.php
+++ views/menu.php
@@ -32,8 +32,8 @@
     <div class="redes_menu"><img src="img/logo_ig.png" class="img_redes_menu" id="ig" alt=""><img src="img/logo_tt.png" class="img_redes_menu" alt=""><img src="img/logo_tw.png" class="img_redes_menu" alt=""></div>
     <div id="btn_descargar">Descargar</div>
 </div>
--->
 
+-->
 <nav class="navbar navbar-expand-lg navbar-light  navbar-custom" aria-label="Fifth navbar example">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
@@ -67,3 +67,52 @@
         </div>
     </div>
 </nav>
+
+<!--
+<nav class="navbar navbar-expand-lg navbar-light  navbar-custom" aria-label="Fifth navbar example">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+            <div class="img_logo">
+                <img src="img/logo_JoyBox.png" alt="JoyBox" class="logo">
+                <img src="img/JoyBox.png" alt="" class="n_logo">
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExample05">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link <?php echo($view == "home"? "activo" : "no-activo");?>" aria-current="page" href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link <?php echo($view == "sobre_nosotros"? "activo" : "no-activo");?>" aria-current="page" href="sobre_nosotros.php">Sobre Nosotros</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link <?php echo($view == "contacto"? "activo" : "no-activo");?>" aria-current="page" href="contacto.php">Contacto</a>
+                </li>
+            </ul>
+            <div class="redes">
+                <i class="redes_menu bi bi-instagram"></i>
+                <i class="redes_menu bi bi-tiktok red"></i>
+                <i class="redes_menu bi bi-twitter"></i>
+            </div>
+
+            <button type="button" class="btn btn-outline-light descarga-bnt">Descargar</button>
+        </div>
+    </div>
+</nav> 
+
+<div class="contenedor-inicio">
+    <div class="contenedor-imagen">
+        <img src="img/mancha_roja.png" alt="" class="mancha-roja">
+    </div>
+    <div class="text-inicio">
+        <p>El camino</p>
+        <p>del éxito</p>
+        <p>comienza aquí</p>
+    </div>
+</div>
+
+<img src="img/dados.png" alt="" class="dados">
+-->
