09d1696ec3ca388013d04934e1f319bb4cd91615
angieVentura <angiev.d.23@gmail.com>
2023-04-30 14:30:56 -0300

    Arreglo problema de los div

--- views/home.php
+++ views/home.php
@@ -1,37 +1,35 @@
-<div class="seccion-dados">
-    <div class="contenedor-imagen">
-        <img src="../JoyBox/img/mancha_r.png" alt="Mancha roja" class="mancha-roja">
+<div class="contenedor-imagen">
+    <img src="../JoyBox/img/mancha_r.png" alt="Mancha roja" class="mancha-roja">
+</div>
+
+<div class="contenedor-inicio">
+    <div class="text-inicio">
+        <h1>DONDE COMIENZA <br>TU DIVERSÍON</h1>
     </div>
-    <div class="contenedor-inicio">
-        <div class="text-inicio">
-            <h1>DONDE COMIENZA <br>TU DIVERSÍON</h1>
-        </div>
-        <div class="img-inicio">
-            <img src="../JoyBox/img/dados.png" alt="" class="dados">
-        </div>
+    <div class="img-inicio">
+        <img src="../JoyBox/img/dados.png" alt="" class="dados">
     </div>
 </div>
 
-<div class="seccion-cartas">
-    <div class="contenedor-inicio-cartas">
-        <div class="img-inicio-cartas">
-            <img src="../JoyBox/img/mancha_negra.png" alt="" class="cartas">
-        </div>
-        <div class="text-inicio-cartas">
-            <h2 class="cartas">¿QUÉ ES JOYBOX?</h2>
-            <p class="carta">JoyBox es una app de emocionantes juegos de amplia variedad. Podrás tener acceso a los mejores juegos en una sola plataforma fácil de usar.</p>
-        </div>
+<div class="contenedor-inicio-cartas">
+    <div class="img-inicio-cartas">
+        <img src="../JoyBox/img/mancha_negra.png" alt="" class="cartas">
+    </div>
+    <div class="text-inicio-cartas">
+        <h2 class="cartas">¿QUÉ ES JOYBOX?</h2>
+        <p class="carta">JoyBox es una app de emocionantes juegos de amplia variedad. Podrás tener acceso a los mejores juegos en una sola plataforma fácil de usar.</p>
     </div>
 </div>
-<!--
-<div class="seccion-juegos">
-    <div class="contenedor-inicio-juegos">
-        <div class="text-inicio-juegos">
-            <p class="juego">JoyBox es una plataforma en línea que ofrece <br> una gran variedad de juegos, como <br>Chess, Tetris y Truco. Tendrás la opción de jugar<br> solo o con amigos, así como crear <br> comunidades y grupos para conectarte <br>con otros jugadores.
-            </p>
-        </div>
-        <div class="img-inicio-juegos">
-            <img src="../JoyBox/img/manchita_juegos.png" alt="" class="juegos">
-        </div>
+
+<div class="contenedor-inicio-juegos">
+    <div class="text-inicio-juegos">
+        <p class="juego">JoyBox es una plataforma que ofrece<br> una gran variedad de juegos, como <br>Chess, Tetris y Truco. Tendrás la opción<br> de jugar solo o con amigos, así como <br>crear  comunidades y grupos para <br>conectarte con otros jugadores.
+        </p>
     </div>
-</div>-->
+    <div class="img-inicio-juegos">
+        <img src="../JoyBox/img/manchita_juegos.png" alt="" class="juegos">
+    </div>
+</div>
+srrtrrthrth
+<!--
+-->
--- views/layout.php
+++ views/layout.php
@@ -9,9 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
     <title>JoyBox</title>
+    <link rel="icon" href="../JoyBox/img/logo_JoyBox.png" type="image/png">
 </head>
 
 <body>
@@ -19,12 +19,12 @@
         <?php require_once "views/menu.php"; ?>
     </header>
 
-    <div class="contenedor">
+
         <?php
         $view = (isset($view) ? $view : "home");
         require_once $view . ".php";
         ?>
-    </div>
+
     <footer class="c_footer text-light py-3">
         <?php require_once "views/footer.php"; ?>
     </footer>
