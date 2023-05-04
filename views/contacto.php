cabbb76946a608130625c9be3f02813a22b3e08e
Tobitobit0bi <tobias.coman@hotmail.com>
2023-05-04 17:46:15 -0300

    avanzo en seccion contacto

--- views/contacto.php
+++ views/contacto.php
@@ -1,5 +1,12 @@
 <h5 class="tituloC"> Hola, JoyBox: </h5>
-<div class="mancha-gris"> <div class="caja-negra-gris"> <div class="gris"></div> </div> </div>
-<div class="mancha-carta"></div>
+<div class="caja-principal-contacto">
+    <div class="mancha-gris"> 
+        <div class="caja-negra-gris"> 
+            <div class="gris"> </div> 
+        </div> 
+    </div>
+    <div class="mancha-carta"> <img src ="img/manchaNegraCarta.png"> </div>
+</div>
+
 
 
