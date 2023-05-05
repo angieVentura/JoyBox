cebd06e5beb3235086bbf298586c330b961e510b
Tobitobit0bi <tobias.coman@hotmail.com>
2023-05-05 11:51:04 -0300

    avances en la estructura y controladores de seción contactos

--- views/contacto.php
+++ views/contacto.php
@@ -1,11 +1,23 @@
-<h5 class="tituloC"> Hola, JoyBox: </h5>
+
 <div class="caja-principal-contacto">
     <div class="mancha-gris"> 
+    <h5 class="tituloC"> Hola, JoyBox: </h5>
         <div class="caja-negra-gris"> 
-            <div class="gris"> </div> 
+            <div class="gris">
+                <form action="contacto.php" method="POST">
+                    <div class="caja-texto">
+                        <input type="text" class="input-texto" name="email" id="email" placeholder="email" required></input>
+                        <label for="email" class="label-input-texto">Email</label>
+                    </div>
+                    
+                    <input type="text" id="titulo"></input>
+                    <input type="text" id="prosa"></input>
+
+                </form>
+                    
+            </div> 
         </div> 
     </div>
-    <div class="mancha-carta"> <img src ="img/manchaNegraCarta.png"> </div>
 </div>
 
 
