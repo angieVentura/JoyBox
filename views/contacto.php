e90d2a7901898ff22db50912f3afb52e2af4cd2b
TobiTobiT0bi <Tobias.coman@hotmail.com>
2023-05-08 02:20:45 -0300

    termino con la creacion de la base de la seccion contacto (FALTA COMPLETAR ESTILOS)

--- views/contacto.php
+++ views/contacto.php
@@ -1,24 +1,34 @@
+<body class="body-contacto">
+    <div class="caja-principal-contacto">
+        <div class="mancha-gris"> 
+        <h5 class="tituloC"> Hola, JoyBox: </h5>
+            <div class="caja-negra-gris"> 
+                <div class="gris">
+                    <form action="contacto.php" method="POST">
+                        <div class="col-3">
+                            <input class="effect-input" type="text" placeholder="Email" name="email" required>
+                            <span class="focus-border"></span>
+                        </div>
 
-<div class="caja-principal-contacto">
-    <div class="mancha-gris"> 
-    <h5 class="tituloC"> Hola, JoyBox: </h5>
-        <div class="caja-negra-gris"> 
-            <div class="gris">
-                <form action="contacto.php" method="POST">
-                    <div class="caja-texto">
-                        <input type="text" class="input-texto" name="email" id="email" placeholder="email" required></input>
-                        <label for="email" class="label-input-texto">Email</label>
-                    </div>
-                    
-                    <input type="text" id="titulo"></input>
-                    <input type="text" id="prosa"></input>
+                        <div class="col-3">
+                            <input class="effect-input" type="text" placeholder="Titulo" name="titulo" required>
+                            <span class="focus-border"></span>
+                        </div>
 
-                </form>
-                    
+                        <div class="col-3">
+                            <textarea class="effect-input" type="text" placeholder="Explica aqui el porqué te quieres contactar" name="prosa" required></textarea>
+                            <span class="focus-border"></span>
+                        </div>
+
+                        <div class="col-sm-3">
+                            <button type="submit" class="btn btn-danger">Enviar</button>
+                        </div>
+                    </form>                       
+                </div> 
             </div> 
-        </div> 
+        </div>
     </div>
-</div>
+</body>
 
 
 
