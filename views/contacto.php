0199617b3598578d296a8b0da5ffb6e626a7b5c8
tobitobitobi <tobias.coman@hotmail.com>
2023-05-12 15:28:32 -0300

    avances contacto

--- views/contacto.php
+++ views/contacto.php
@@ -4,26 +4,28 @@
         <h5 class="tituloC"> Hola, JoyBox: </h5>
             <div class="caja-negra-gris"> 
                 <div class="gris">
-                    <form action="contacto.php" method="POST">
-                        <div class="col-3">
-                            <input class="effect-input" type="text" placeholder="Email" name="email" required>
-                            <span class="focus-border"></span>
-                        </div>
+                    <div class="form">
+                        <form action="contacto.php" method="POST">
+                            <div class="col-3 first">
+                                <input class="effect-input" type="text" placeholder="Email" name="email" required>
+                                <span class="focus-border"></span>
+                            </div>
 
-                        <div class="col-3">
-                            <input class="effect-input" type="text" placeholder="Titulo" name="titulo" required>
-                            <span class="focus-border"></span>
-                        </div>
+                            <div class="col-3">
+                                <input class="effect-input" type="text" placeholder="Titulo" name="titulo" required>
+                                <span class="focus-border"></span>
+                            </div>
 
-                        <div class="col-3">
-                            <textarea class="effect-input" type="text" placeholder="Explica aqui el porqué te quieres contactar" name="prosa" required></textarea>
-                            <span class="focus-border"></span>
-                        </div>
+                            <div class="col-3 area">
+                                <textarea class="effect-input area" type="text" placeholder="Explica aqui el porqué te quieres contactar" name="prosa" required></textarea>
+                                <span class="focus-border"></span>
+                            </div>
 
-                        <div class="col-sm-3">
-                            <button type="submit" class="btn btn-danger">Enviar</button>
-                        </div>
-                    </form>                       
+                            <div class="col-sm-3">
+                                <button type="submit" class="btn btn-danger">Enviar</button>
+                            </div>
+                        </form>
+                    </div>                       
                 </div> 
             </div> 
         </div>
