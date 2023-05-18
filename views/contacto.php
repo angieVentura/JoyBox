7cb519958574c4c8f4b2200f403eccc2e722d901
rojastamaraa <rojastamaranya@gmail.com>
2023-05-17 22:38:24 -0300

    seccion contacto

--- views/contacto.php
+++ views/contacto.php
@@ -1,34 +1,23 @@
-<body class="body-contacto">
-    <div class="caja-principal-contacto">
-        <div class="mancha-gris"> 
-        <h5 class="tituloC"> Hola, JoyBox: </h5>
-            <div class="caja-negra-gris"> 
-                <div class="gris">
-                    <form action="contacto.php" method="POST">
-                        <div class="col-3">
-                            <input class="effect-input" type="text" placeholder="Email" name="email" required>
-                            <span class="focus-border"></span>
-                        </div>
-
-                        <div class="col-3">
-                            <input class="effect-input" type="text" placeholder="Titulo" name="titulo" required>
-                            <span class="focus-border"></span>
-                        </div>
-
-                        <div class="col-3">
-                            <textarea class="effect-input" type="text" placeholder="Explica aqui el porqué te quieres contactar" name="prosa" required></textarea>
-                            <span class="focus-border"></span>
-                        </div>
-
-                        <div class="col-sm-3">
-                            <button type="submit" class="btn btn-danger">Enviar</button>
-                        </div>
-                    </form>                       
-                </div> 
-            </div> 
-        </div>
+<div class="cont-bckg">
+    <div class="container-sm">
+        <h1 class="hj">¡Hola Joybox!</h1>
+        <form action="contacto.php" method="POST" class="fofo">
+            <div class="input-group mb-3">
+                <input type="text" class="effect-input" type="text" placeholder="Email" name="email" required>
+                <span class="focus-border"></span>
+            </div>
+            <div class="input-group mb-3">
+                <input class="effect-input" type="text" placeholder="Titulo" name="titulo" required>
+                <span class="focus-border"></span>
+            </div>
+            <div class="input-group mb-3">
+                <textarea class="effect-input" type="text" placeholder="Explica aqui el porqué te quieres contactar" name="prosa" required></textarea>
+                <span class="focus-border"></span>
+            </div>
+            <div class="col-sm-3">
+                <button type="submit" class="btn btn-danger">Enviar</button>
+            </div>
+        </form>
     </div>
-</body>
-
-
+</div>
 
