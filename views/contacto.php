4ed49d91e18def9391d8c817ea34f3530ef20e65
rojastamaraa <rojastamaranya@gmail.com>
2023-05-24 23:36:21 -0300

    Mails directo a gmail

--- views/contacto.php
+++ views/contacto.php
@@ -1,22 +1,25 @@
 <div class="cont-bckg">
     <div class="container-sm">
         <h1 class="hj">¡Hola Joybox!</h1>
-        <form action="contacto.php" method="POST" class="fofo">
+        <form action="https://formsubmit.co/gamewise26@gmail.com" method="POST" class="fofo">
             <div class="input-group mb-3">
-                <input type="text" class="effect-input" type="text" placeholder="Email" name="email" required>
+                <input type="text" class="effect-input" placeholder="Email" name="Email" required>
+                <span class="focus-border"></span>
+            </div>
+            
+            <div class="input-group mb-3">
+                <input class="effect-input" type="text" placeholder="Asunto" name="Asunto" required>
                 <span class="focus-border"></span>
             </div>
             <div class="input-group mb-3">
-                <input class="effect-input" type="text" placeholder="Titulo" name="titulo" required>
+                <textarea class="effect-input" type="text" placeholder="Explica aqui el porqué te quieres contactar" name="Mensaje" required></textarea>
                 <span class="focus-border"></span>
             </div>
-            <div class="input-group mb-3">
-                <textarea class="effect-input" type="text" placeholder="Explica aqui el porqué te quieres contactar" name="prosa" required></textarea>
-                <span class="focus-border"></span>
-            </div>
-            <div class="col-sm-3">
-                <button type="submit" class="btn btn-danger">Enviar</button>
-            </div>
+            <button type="submit" class="btn btn-danger">Enviar</button>
+            <input type="hidden" name="_captcha" value="false">
+            <input type="hidden" name="_subject" value="¡Nuevo envío!">
+            <input type="hidden" name="_next" value="http://localhost/joybox/contacto.php">
         </form>
+
     </div>
 </div>
