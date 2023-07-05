4f09393831e768095b2b7db921c18862cc3b3c57
rojastamaraa <rojastamaranya@gmail.com>
2023-07-05 20:55:34 -0300

    rediseño contacto me olvide de crear otra rama

--- views/contacto.php
+++ views/contacto.php
@@ -1,18 +1,25 @@
-<div class="cont-bckg">
+<div class="container coco">
     <div class="container-sm">
-        <h1 class="hj">¡Hola Joybox!</h1>
-        <form action="https://formsubmit.co/gamewise26@gmail.com" method="POST" class="fofo">
+        <div class="manolito">
+
+        </div>
+        <form action="https://formsubmit.co/gamewise26@gmail.com" method="POST" class="formucon">
+            <h3 class="hj">¡HOLA JOYBOX!</h3>
+            <label class="labin" for="email">Email</label>
             <div class="input-group mb-3">
-                <input type="text" class="effect-input" placeholder="Email" name="Email" required>
+                <input type="text" class="effect-input" name="Email" id="email" required>
                 <span class="focus-border"></span>
             </div>
-            
+
+            <label class="labin" for="asunto">Asunto</label>
             <div class="input-group mb-3">
-                <input class="effect-input" type="text" placeholder="Asunto" name="Asunto" required>
+                <input class="effect-input" type="text" name="Asunto" id="asunto" required>
                 <span class="focus-border"></span>
             </div>
+
+            <label class="labin" for="mensaje">Mensaje</label>
             <div class="input-group mb-3">
-                <textarea class="effect-input" type="text" placeholder="Explica aqui el porqué te quieres contactar" name="Mensaje" required></textarea>
+                <textarea class="effect-input" type="text" name="Mensaje" id="mensaje" required></textarea>
                 <span class="focus-border"></span>
             </div>
             <button type="submit" class="btn btn-danger">Enviar</button>
@@ -20,6 +27,5 @@
             <input type="hidden" name="_subject" value="¡Nuevo envío!">
             <input type="hidden" name="_next" value="http://localhost/joybox/contacto.php">
         </form>
-
     </div>
 </div>
